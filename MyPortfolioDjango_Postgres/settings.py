9b8ea1f26aff3af522d404c515229571865ab1a0
Lê Hoàng Huy Thanh <93971600+lehoanghuythanh96@users.noreply.github.com>
2022-05-09 15:11:20 +0200

    set up dotenv file

--- MyPortfolioDjango_Postgres/settings.py
+++ MyPortfolioDjango_Postgres/settings.py
@@ -9,8 +9,13 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+import os
 from datetime import timedelta
 from pathlib import Path
+
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-yfg@3!e+_$4s1%-yr%1-nbw&f^4u@y!b8^il9+sb9g7l)1+yk='
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
